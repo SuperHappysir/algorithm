17678ef7d7d42856a3adf4cb24f5181274f6b0cd
luotao <luotao@gutou.com>
2019-01-15 18:02:09 +0800

    整理格式

--- subSet/main.php
+++ subSet/main.php
@@ -9,24 +9,24 @@
  * @usage php main.php
  */
 function subsets($array) : array
-    {
-        $n        = count($array);
-        $subN     = 2 ** $n;
-        $subArray = [];
+{
+    $n        = count($array);
+    $subN     = 2 ** $n;
+    $subArray = [];
 
-        $subArray[] = [];
-        for ($i = 1; $i < $subN; $i++) {
-            $m    = sprintf('%0+' . $n . 'b', $i);
-            $tArr = [];
-            for ($j = 0; $j < $n; $j++) {
-                if ($m{$j} == 1) {
-                    $tArr[] = $array[$j];
-                }
+    $subArray[] = [];
+    for ($i = 1; $i < $subN; $i++) {
+        $m    = sprintf('%0+' . $n . 'b', $i);
+        $tArr = [];
+        for ($j = 0; $j < $n; $j++) {
+            if ($m{$j} == 1) {
+                $tArr[] = $array[$j];
             }
-            $subArray[] = $tArr;
         }
-
-        return $subArray;
+        $subArray[] = $tArr;
     }
 
-    var_dump(subsets([1, 2, 3]));
+    return $subArray;
+}
+
+var_dump(subsets([1, 2, 3]));
