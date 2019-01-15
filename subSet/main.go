18737de3031f53bcdabb08ad37badc62ca4c084e
luotao <luotao@gutou.com>
2019-01-15 18:28:11 +0800

    添加注释

--- subSet/main.go
+++ subSet/main.go
@@ -23,8 +23,11 @@ func subSet(arr []interface{}) [][]interface{} {
 
 	subArray = append(subArray, nullArr)
 
+	// 共有maxCount个子集
 	for i := 1; i < maxCount; i++ {
 		var tArr = nullArr
+		// 获取当前子集的实际组合
+		// 这里遍历数组是为了将当前外层循环的可能性转换成实际组合数组
 		for j := 0; j <= num; j++ {
 			if (i & (1 << uint(j))) != 0 {
 				tArr = append(tArr, arr[j])
