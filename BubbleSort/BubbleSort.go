b1369f2700361d2f4413c24334653fe6715768db
SuperHappysir <2417599488@qq.com>
2019-10-24 21:55:28 +0800

    -m优化冒泡排序

--- BubbleSort/BubbleSort.go
+++ BubbleSort/BubbleSort.go
@@ -13,20 +13,36 @@ import "sort"
 // 持续每次对越来越少的元素重复上面的步骤，直到没有任何一对数字需要比较。
 
 func BubbleSort(array sort.IntSlice) sort.IntSlice {
-
-	length := len(array)
-
-	// 循环冒泡倒数第index位
-	// 第一次需要冒泡到length位，需要对比的最后位置为length-1,length
-	for index := 0; index < length-1; index++ {
-		// 两两对比，将最大的数冒泡到倒数第length - index - 1位（length - index - 1是为了排除已冒泡完成的位）
-		for subIndex := 0; subIndex < length-index-1; subIndex++ {
-			// 如果前一位的数比后一位的数大，就交换他们两个
-			if array[subIndex] > array[subIndex+1] {
-				array.Swap(subIndex, subIndex+1)
-			}
-		}
-	}
-
-	return array
+    
+    length := len(array)
+    
+    /*
+        依次比较每一对相邻元素，如有必要，交换之
+        若整趟扫描都没有进行交换，则排序完成，否则再做一趟扫描交换
+    */
+    sorted := false
+    for ; !sorted; length-- { // 逐趟扫描交换,直至完全有序
+        sorted = true                                        // 我们假设默认全局有序
+        for subIndex := 0; subIndex < length-1; subIndex++ { // 自左向右，逐渐检查A[0,n)内各相邻元素
+            // 如果前一位的数比后一位的数大（逆序）
+            if array[subIndex] > array[subIndex+1] {
+                array.Swap(subIndex, subIndex+1) // 就交换他们两个
+                sorted = false                   // 清楚全局有序标志
+            }
+        }
+    }
+    
+    // // 循环冒泡倒数第index位
+    // // 第一次需要冒泡到length位，需要对比的最后位置为length-1,length
+    // for index := 0; index < length-1; index++ {
+    // 	// 两两对比，将最大的数冒泡到倒数第length - index - 1位（length - index - 1是为了排除已冒泡完成的位）
+    // 	for subIndex := 0; subIndex < length-index-1; subIndex++ {
+    // 		// 如果前一位的数比后一位的数大，就交换他们两个
+    // 		if array[subIndex] > array[subIndex+1] {
+    // 			array.Swap(subIndex, subIndex+1)
+    // 		}
+    // 	}
+    // }
+    
+    return array
 }
