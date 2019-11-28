83524d30e7a9f8ec641fb690cf4fefd3b5fab856
SuperHappysir <2417599488@qq.com>
2019-11-28 15:16:39 +0800

    冒泡排序一种很好的写法【来自清华邓俊辉教授02-E-4节】

--- BubbleSort/BubbleSort.go
+++ BubbleSort/BubbleSort.go
@@ -13,36 +13,60 @@ import "sort"
 // 持续每次对越来越少的元素重复上面的步骤，直到没有任何一对数字需要比较。
 
 func BubbleSort(array sort.IntSlice) sort.IntSlice {
-    
-    length := len(array)
-    
-    /*
-        依次比较每一对相邻元素，如有必要，交换之
-        若整趟扫描都没有进行交换，则排序完成，否则再做一趟扫描交换
-    */
-    sorted := false
-    for ; !sorted; length-- { // 逐趟扫描交换,直至完全有序
-        sorted = true                                        // 我们假设默认全局有序
-        for subIndex := 0; subIndex < length-1; subIndex++ { // 自左向右，逐渐检查A[0,n)内各相邻元素
-            // 如果前一位的数比后一位的数大（逆序）
-            if array[subIndex] > array[subIndex+1] {
-                array.Swap(subIndex, subIndex+1) // 就交换他们两个
-                sorted = false                   // 清楚全局有序标志
-            }
-        }
-    }
-    
-    // // 循环冒泡倒数第index位
-    // // 第一次需要冒泡到length位，需要对比的最后位置为length-1,length
-    // for index := 0; index < length-1; index++ {
-    // 	// 两两对比，将最大的数冒泡到倒数第length - index - 1位（length - index - 1是为了排除已冒泡完成的位）
-    // 	for subIndex := 0; subIndex < length-index-1; subIndex++ {
-    // 		// 如果前一位的数比后一位的数大，就交换他们两个
-    // 		if array[subIndex] > array[subIndex+1] {
-    // 			array.Swap(subIndex, subIndex+1)
-    // 		}
-    // 	}
-    // }
-    
-    return array
+
+	length := len(array)
+
+	/*
+	   依次比较每一对相邻元素，如有必要，交换之
+	   若整趟扫描都没有进行交换，则排序完成，否则再做一趟扫描交换
+	*/
+	sorted := false
+	for ; !sorted; length-- { // 逐趟扫描交换,直至完全有序
+		sorted = true                                        // 我们假设默认全局有序
+		for subIndex := 0; subIndex < length-1; subIndex++ { // 自左向右，逐渐检查A[0,n)内各相邻元素
+			// 如果前一位的数比后一位的数大（逆序）
+			if array[subIndex] > array[subIndex+1] {
+				array.Swap(subIndex, subIndex+1) // 就交换他们两个
+				sorted = false                   // 清楚全局有序标志
+			}
+		}
+	}
+
+	// // 循环冒泡倒数第index位
+	// // 第一次需要冒泡到length位，需要对比的最后位置为length-1,length
+	// for index := 0; index < length-1; index++ {
+	// 	// 两两对比，将最大的数冒泡到倒数第length - index - 1位（length - index - 1是为了排除已冒泡完成的位）
+	// 	for subIndex := 0; subIndex < length-index-1; subIndex++ {
+	// 		// 如果前一位的数比后一位的数大，就交换他们两个
+	// 		if array[subIndex] > array[subIndex+1] {
+	// 			array.Swap(subIndex, subIndex+1)
+	// 		}
+	// 	}
+	// }
+
+	return array
+}
+
+func BubbleSort2(array sort.IntSlice) sort.IntSlice {
+
+	hi := len(array)
+	// while(!bubble(array, 9, hi--))
+	for !bubble(array, 0, hi) { // 逐趟做扫描交换，直至全序
+		hi--
+	}
+	return array
+}
+
+func bubble(array sort.IntSlice, lo, hi int) bool {
+	sorted := true // 全局有序标志（用于做冒泡排序的优化，当对未排序序列做完一趟扫描后发现全部有序，那么剩下的就不用排序了，已经达到了全局有序的目的）
+	// 同其他语言: while (++lo < hi) {}
+	for lo++; lo < hi; lo++ {
+		if array[lo-1] > array[lo] { // 若为逆序，进行交换
+			sorted = false
+			//  swap
+			array[lo-1], array[lo] = array[lo], array[lo-1]
+		}
+	}
+
+	return sorted
 }
--- BubbleSort/BubbleSort_test.go
+++ BubbleSort/BubbleSort_test.go
@@ -33,3 +33,29 @@ func TestBubbleSort(t *testing.T) {
 	fmt.Println(array2)
 
 }
+
+func TestBubbleSort2(t *testing.T) {
+	random := rand.New(rand.NewSource(time.Now().UnixNano()))
+	array1 := make([]int, random.Intn(20))
+	for i := range array1 {
+		array1[i] = random.Intn(100)
+	}
+
+	fmt.Println(array1)
+
+	array2 := make(sort.IntSlice, len(array1))
+	copy(array2, array1)
+
+	BubbleSort2(array1)
+
+	array2.Sort()
+	for i := range array1 {
+		if array1[i] != array2[i] {
+			t.Fail()
+		}
+	}
+
+	fmt.Println(array1)
+	fmt.Println(array2)
+
+}
